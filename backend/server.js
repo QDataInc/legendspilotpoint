4dcf34210b97c65a8730cad432b9360ef754812d
AkankshaShetty7 <akankshashetty7@gmail.com>
2025-05-19 22:15:09 -0500

    endpoint changes

--- backend/server.js
+++ backend/server.js
@@ -146,7 +146,7 @@ app.get('/api/available-rooms', async (req, res) => {
     const { data: rooms, error: roomsError } = await supabase
       .from('rooms')
       .select('id, room_type, max_occupancy, price_per_night, image')
-      .eq('room_type', room_type);
+      .ilike('room_type', room_type);
 
     if (roomsError) throw roomsError;
 
