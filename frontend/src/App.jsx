ed6a85722e3490228c02451580097078a5466ad9
AkankshaShetty7 <akankshashetty7@gmail.com>
2025-05-27 16:31:42 -0500

    navbar changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -153,7 +153,7 @@ function App() {
           </div>
         )}
         <Navbar className="flex flex-col min-h-screen"></Navbar>
-        <div className="flex flex-col min-h-screen pt-20 lg:pt-24">
+        <div className="flex flex-col min-h-screen">
           <ScrollToTop />
           <main className="flex-grow">
             <Routes>
